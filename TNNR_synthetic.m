67396c51de70a71cdcb309a113f38e0cd3819335
xueshengke <xueshengke1993@163.com>
2017-05-09 15:36:53 +0800

    add synthetic experiment

--- TNNR_synthetic.m
+++ TNNR_synthetic.m
@@ -21,26 +21,13 @@ admm_result = './TNNR-admm/result/image';
 apgl_result = './TNNR-apgl/result/image';
 if ~exist(admm_result, 'dir'),   mkdir(admm_result); end
 if ~exist(apgl_result, 'dir'),   mkdir(apgl_result); end
-image_list = {'re1.jpg', 're2.jpg', 're3.jpg', 're4.jpg', 're5.jpg', ...
-              're6.jpg', 're7.jpg', 're8.jpg', 're9.jpg', 're10.jpg', ...
-              're11.jpg' };
-
-file_list = dir('mask');
-num_mask = length(file_list) - 2;
-mask_list = cell(num_mask, 1);
-for i = 1 : num_mask
-    mask_list{i} = file_list(i+2).name;
-end
 
 %% parameter configuration
-image_id = 9;            % select an image for experiment
-mask_id  = 6;            % select a mask for experiment
-
-para.block = 0;          % 1 for block occlusion, 0 for random noise
-para.lost = 0.10;        % percentage of lost elements in matrix
-para.save_eps = 1;       % save eps figure in result directory
-para.min_R =  1;         % minimum rank of chosen image
-para.max_R = 10;         % maximum rank of chosen image
+% para.block = 1;          % 1 for block occlusion, 0 for random noise
+para.lost = 0.50;        % percentage of lost elements in matrix
+para.save_eps = 0;       % save eps figure in result directory
+para.min_R = 1;          % minimum rank of chosen image
+para.max_R = 20;          % maximum rank of chosen image
 % it requires to test all ranks from min_R to max_R, note that different
 % images have different ranks, and various masks affect the ranks, too.
 
@@ -56,26 +43,29 @@ para.apgl_tol = 1e-4;    % epsilon of the APGL optimization
 para.apgl_lambda = 1e-2; % lambda of the the APGL optimization
 para.progress = 0;
 
-%% select an image and a mask for experiment
-image_name = image_list{image_id};
-X_full = double(imread(image_name));
-[m, n, dim] = size(X_full);
-fprintf('choose image: %s, ', image_name);
+%% generate synthetic data for experiment
+image_name = 'synthetic_data';
+m = 200;
+n = 200;
+dim = 3;
+r = 10;
+sigma = 0.1;    % [0.1, 0.9]
 
-if para.block  
-    % block occlusion
-    mask = double(imread(mask_list{mask_id}));
-    mask = mask ./ max(mask(:));       % index matrix of the known elements
-    fprintf('mask: %s.\n', mask_list{mask_id});
-else
-    % random loss
-    rnd_idx = randi([0, 100-1], m, n);
-    old_idx = rnd_idx;
-    lost = para.lost * 100;
-    fprintf('loss: %d%% elements are missing.\n', lost);
-    rnd_idx = double(old_idx < (100-lost));
-    mask = repmat(rnd_idx, [1 1 dim]); % index matrix of the known elements
-end
+%% random loss
+rnd_idx = randi([0, 100-1], m, n);
+old_idx = rnd_idx;
+lost = para.lost * 100;
+fprintf('loss: %d%% elements are missing.\n', lost);
+rnd_idx = double(old_idx < (100-lost));
+mask = repmat(rnd_idx, [1 1 dim]); % index matrix of the known elements
+% missing = ones(size(mask)) - mask; % index matrix of the unknown elements
+
+L = rand(m, r);
+R = rand(n, r);
+noise = sigma * rand(m, n);
+M = L * R' + noise * mask(:,:,1);
+M = mapminmax(M, 0, 255);
+X_full = repmat(M, [1 1 dim]);
 
 %% run truncated nuclear norm regularization through ADMM
 fprintf('ADMM optimization method to recovery an image with missing pixels\n');
@@ -84,7 +74,7 @@ t1 = tic;
 
 admm_rank = admm_res.best_rank;
 admm_psnr = admm_res.best_psnr;
-admm_erec = admm_res.best_erec;
+admm_erec = admm_res.best_erec ./ 255;
 admm_time_cost = admm_res.time(admm_rank);
 admm_iteration = admm_res.iterations(admm_rank, :);
 admm_total_iter = admm_res.total_iter(admm_rank, :);
@@ -103,7 +93,7 @@ xlabel('Rank');
 ylabel('PSNR');
 
 subplot(2, 2, 2);
-plot(admm_res.Rank, admm_res.Erec, 'diamond-');
+plot(admm_res.Rank, admm_res.Erec ./ 255, 'diamond-');
 xlabel('Rank');
 ylabel('Recovery error');
 
@@ -113,7 +103,7 @@ xlabel('Iteration');
 ylabel('PSNR');
 
 subplot(2, 2, 4);
-plot(admm_res.Erec_iter, '^-');
+plot(admm_res.Erec_iter ./ 255, '^-');
 xlabel('Iteration');
 ylabel('Recovery error');
 
@@ -132,10 +122,10 @@ outputFileName = fullfile(admm_result, 'parameters.txt');
 fid = fopen(outputFileName, 'a') ;
 fprintf(fid, '****** %s ******\n', datestr(now,0));
 fprintf(fid, '%s\n', ['image: '           image_name               ]);
-fprintf(fid, '%s\n', ['mask: '            mask_list{mask_id}       ]);
-fprintf(fid, '%s\n', ['block or noise: '  num2str(para.block)      ]);
+% fprintf(fid, '%s\n', ['mask: '            mask_list{mask_id}       ]);
+% fprintf(fid, '%s\n', ['block or noise: '  num2str(para.block)      ]);
 fprintf(fid, '%s\n', ['loss ratio: '      num2str(para.lost)       ]);
-fprintf(fid, '%s\n', ['save eps figure: ' num2str(para.save_eps)   ]);
+% fprintf(fid, '%s\n', ['save eps figure: ' num2str(para.save_eps)   ]);
 fprintf(fid, '%s\n', ['min rank: '        num2str(para.min_R)      ]);
 fprintf(fid, '%s\n', ['max rank: '        num2str(para.max_R)      ]);
 fprintf(fid, '%s\n', ['max iteration: '   num2str(para.outer_iter) ]);
@@ -162,7 +152,7 @@ t2 = tic;
 
 apgl_rank = apgl_res.best_rank;
 apgl_psnr = apgl_res.best_psnr;
-apgl_erec = apgl_res.best_erec;
+apgl_erec = apgl_res.best_erec ./ 255;
 apgl_time_cost = apgl_res.time(apgl_rank);
 apgl_iteration = apgl_res.iterations(apgl_rank, :);
 apgl_total_iter = apgl_res.total_iter(apgl_rank, :);
@@ -181,7 +171,7 @@ xlabel('Rank');
 ylabel('PSNR');
 
 subplot(2, 2, 2);
-plot(apgl_res.Rank, apgl_res.Erec, 'diamond-');
+plot(apgl_res.Rank, apgl_res.Erec ./ 255, 'diamond-');
 xlabel('Rank');
 ylabel('Recovery error');
 
@@ -191,7 +181,7 @@ xlabel('Iteration');
 ylabel('PSNR');
 
 subplot(2, 2, 4);
-plot(apgl_res.Erec_iter, '^-');
+plot(apgl_res.Erec_iter ./ 255, '^-');
 xlabel('Iteration');
 ylabel('Recovery error');
 
@@ -210,10 +200,10 @@ outputFileName = fullfile(apgl_result, 'parameters.txt');
 fid = fopen(outputFileName, 'a') ;
 fprintf(fid, '****** %s ******\n', datestr(now,0));
 fprintf(fid, '%s\n', ['image: '           image_name               ]);
-fprintf(fid, '%s\n', ['mask: '            mask_list{mask_id}       ]);
-fprintf(fid, '%s\n', ['block or noise: '  num2str(para.block)      ]);
+% fprintf(fid, '%s\n', ['mask: '            mask_list{mask_id}       ]);
+% fprintf(fid, '%s\n', ['block or noise: '  num2str(para.block)      ]);
 fprintf(fid, '%s\n', ['loss ratio: '      num2str(para.lost)       ]);
-fprintf(fid, '%s\n', ['save eps figure: ' num2str(para.save_eps)   ]);
+% fprintf(fid, '%s\n', ['save eps figure: ' num2str(para.save_eps)   ]);
 fprintf(fid, '%s\n', ['min rank: '        num2str(para.min_R)      ]);
 fprintf(fid, '%s\n', ['max rank: '        num2str(para.max_R)      ]);
 fprintf(fid, '%s\n', ['max iteration: '   num2str(para.outer_iter) ]);
